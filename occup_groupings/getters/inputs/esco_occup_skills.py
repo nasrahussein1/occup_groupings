0647ff37b91ed41cd29706b3e1f78ebee0528c36
nasrahussein1 <nasra.hussein@gmail.com>
2021-05-05 18:00:34 +0100

    data getters

---
+++ occup_groupings/getters/inputs/esco_occup_skills.py
@@ -0,0 +1,44 @@
+# File: getters/inputs/esco_skills_occup.py
+"""Data getters for ESCO data and skills and occup data
+"""
+import pandas as pd
+import io
+import boto3
+
+
+def get_occupations() -> pd.DataFrame:
+    """Load occupations.
+
+    Returns:
+        occupations data
+    """
+
+    s3 = boto3.client("s3")
+    obj = s3.get_object(Bucket="occup-groupings-nh", Key="data/raw/occupations_en.csv")
+    return pd.read_csv(io.BytesIO(obj["Body"].read()))
+
+
+def get_skills() -> pd.DataFrame:
+    """Load skills.
+
+    Returns:
+        ESCO skills data
+    """
+    s3 = boto3.client("s3")
+    obj = s3.get_object(Bucket="occup-groupings-nh", Key="data/raw/skills_en.csv")
+    return pd.read_csv(io.BytesIO(obj["Body"].read()))
+
+
+def get_esco_occup_skills() -> pd.DataFrame:
+    """Load esco_occup_skills.
+
+    Returns:
+        ESCO occup_skills data
+    """
+
+    s3 = boto3.client("s3")
+    obj = s3.get_object(
+        Bucket="occup-groupings-nh", Key="data/raw/ESCO_occup_skills.json"
+    )
+
+    return pd.read_json(io.BytesIO(obj["Body"].read()))
