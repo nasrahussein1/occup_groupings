df929a4e762310af5e08707ebd8e95cc44dbdac1
nasrahussein1 <nasra.hussein@gmail.com>
2021-05-05 12:17:02 +0100

    Setup Nesta Data Science cookiecutter

---
+++ docs/index.rst
@@ -0,0 +1,20 @@
+.. toctree::
+   :hidden:
+   :maxdepth: 1
+
+   api/occup_groupings
+   License <license>
+   Github <https://github.com/nestauk/occup_groupings/>
+
+occup_groupings
+===============================
+
+Clustering to create occupation groupings
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ occup_groupings/__init__.py
@@ -0,0 +1,36 @@
+"""occup_groupings."""
+import logging
+import logging.config
+from pathlib import Path
+from typing import Optional
+
+import yaml
+from dotenv import load_dotenv
+
+
+def get_yaml_config(file_path: Path) -> Optional[dict]:
+    """Fetch yaml config and return as dict if it exists."""
+    if file_path.exists():
+        with open(file_path, "rt") as f:
+            return yaml.load(f.read(), Loader=yaml.FullLoader)
+
+
+# Define project base directory
+PROJECT_DIR = Path(__file__).resolve().parents[1]
+
+
+# Read log config file
+_log_config_path = Path("config/logging.yaml")
+_logging_config = get_yaml_config(_log_config_path)
+if _logging_config:
+    logging.config.dictConfig(_logging_config)
+
+# Define module logger
+logger = logging.getLogger(__name__)
+
+# base/global config
+_base_config_path = Path("config/base.yaml")
+config = get_yaml_config(_base_config_path)
+
+# BUCKET and METAFLOW_PROFILE
+load_dotenv(f"{PROJECT_DIR}/.env.shared")
