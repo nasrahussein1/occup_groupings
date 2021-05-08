0b26b9657dc4ffc7c86072ec5fd0377bd900aaff
nasrahussein1 <nasra.hussein@gmail.com>
2021-05-08 18:25:54 +0100

    write plots to s3

--- occup_groupings/utils/to_s3.py
+++ occup_groupings/utils/to_s3.py
@@ -1,6 +1,7 @@
 """functions to write to s3 bucket
 """
 from io import StringIO
+import io
 import tempfile
 import boto3
 import joblib
@@ -37,3 +38,19 @@ def write_model_to_s3(model, bucket, key):
         joblib.dump(model, fp)
         fp.seek(0)
         s3_resource.Object(bucket, key).put(Body=fp.read())
+
+
+def write_plot_to_s3(plot, bucket, key):
+    """write plot to an s3 bucket
+
+    Args:
+        bucket: s3 bucket to write to
+        key: file path for bucket
+        plot: visualisation
+
+    """
+    img_data = io.BytesIO()
+    plot.savefig(img_data, format="png")
+    img_data.seek(0)
+
+    s3_resource.Object(bucket, key).put(Body=img_data)
